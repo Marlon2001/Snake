442608c9db46fa7ca763d8e987743ee831be8396
Marlon Santos <marlonmln.026@gmail.com>
2019-02-23 21:32:59 -0300

    Quase pronto, 90%.

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-import pygame, random
+import pygame, random, time
 from pygame.locals import *
 
 #Cores
@@ -7,49 +7,67 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BORDA = (22, 50, 76)
 
 #Variaveis
 direcao = "ESQUERDA"
 qtdePontos = 0
+qtdeComprimento = 0
+emPause = False
+TAM = 10
+DIS = 1
 
 def on_grid_random():
-    x = random.randint(0,590)
-    y = random.randint(60,590)
-    return (x//10 * 10, y//10 * 10)
+    x = random.randint(3,61)
+    y = random.randint(3,44)
+    #random.randint(4,45)
+    return (x*10, y*10)
 
 def colisaoCelulas(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
 def colisaoBorda(c1):
-    if c1[0] > 590 or c1[0] < 0 or c1[1] > 590 or c1[1] <= 50:
+    if c1[0] > 610 or c1[0] < 30 or c1[1] > 440 or c1[1] < 30:
         return True
-        
 
 #Inicia o pygame
 pygame.init()
-screen = pygame.display.set_mode((600,600))                  #Tamanho da tela
-pygame.display.set_caption('Snake') #Titulo da tela
+screen = pygame.display.set_mode((650,500))  #Tamanho da tela
+pygame.display.set_caption('Snake')          #Titulo da tela
 
-fonte = pygame.font.SysFont('Comis sans ms', 40)
-labelScore = fonte.render('Score: ', 1, WHITE)
+fonteComicBranca = pygame.font.SysFont('Comis sans ms', 25)
+labelScore = fonteComicBranca.render('High Score: ', True, WHITE)
+labelLenght = fonteComicBranca.render('Lenght: ', True, (255, 255, 255))
 
-snake = [(200, 200), (210, 200), (220,200), (230,200)]
-snake_skin = pygame.Surface((10,10))
-snake_skin.fill(WHITE)
+fonteComicVermelha = pygame.font.SysFont('Comic sans ms', 16, False, True)
+labelPause = fonteComicVermelha.render('Jogo em pause, pressione espaço para continuar...', True, RED)
+
+snake = [(200, 200), (201+TAM, 200), (201+TAM*2,200), (201+TAM*3,200)]
+snake_skin = pygame.Surface((TAM,TAM))
+snake_skin.fill(GREEN)
 
 apple_pos = on_grid_random()
-apple = pygame.Surface((10,10))
+apple = pygame.Surface((TAM,TAM))
 apple.fill((255,0,0))
 
-TICK = 10
 clock = pygame.time.Clock()
 while True:
-    clock.tick(TICK)
+    clock.tick(10)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+            quit()
             
         if event.type == KEYDOWN:
+            if event.key == K_1:
+                snake.append((0,0))
+                qtdePontos += 10
+                qtdeComprimento += 1
+            if event.key == K_SPACE:
+                if emPause == False:
+                    emPause = True
+                else:
+                    emPause = False
             if event.key == K_UP:
                 if direcao != "BAIXO":
                     direcao = "CIMA"
@@ -62,44 +80,62 @@ while True:
             if event.key == K_RIGHT:
                 if direcao != "ESQUERDA":
                     direcao = "DIREITA"
+            time.sleep(0.06)
+
+    if emPause == True:
+        screen.blit(labelPause, (260, 0))
+        pygame.display.flip()
+        continue
 
     if colisaoBorda(snake[0]):
-        print('Voce perdeu 1')
         pygame.quit()
-        break
+        quit()
 
     if colisaoCelulas(snake[0], apple_pos):
         qtdePontos += 10
         apple_pos = on_grid_random()
         snake.append((0,0))
+        qtdeComprimento += 1
     
     for i in range(1, len(snake)):
         if(colisaoCelulas(snake[0], snake[i])):
-            print('Voce perdeu 2')
             pygame.quit()
-            break
+            quit()
 
     for i in range(len(snake)-1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
-        
+        if direcao == "CIMA":
+            snake[i] = (snake[i-1][0], snake[i-1][1]+DIS)
+        if direcao == "BAIXO":
+            snake[i] = (snake[i-1][0], snake[i-1][1]-DIS)
+        if direcao == "DIREITA":
+            snake[i] = (snake[i-1][0]-DIS, snake[i-1][1])
+        if direcao == "ESQUERDA":
+            snake[i] = (snake[i-1][0]+DIS, snake[i-1][1])
 
     if direcao == "CIMA":
-        snake[0] = (snake[0][0], snake[0][1] - 10)
+        snake[0] = (snake[0][0], snake[0][1] - TAM)
     if direcao == "BAIXO":
-        snake[0] = (snake[0][0], snake[0][1] + 10)
+        snake[0] = (snake[0][0], snake[0][1] + TAM)
     if direcao == "DIREITA":
-        snake[0] = (snake[0][0] + 10, snake[0][1])
+        snake[0] = (snake[0][0] + TAM, snake[0][1])
     if direcao == "ESQUERDA":
-        snake[0] = (snake[0][0] - 10, snake[0][1])
-    
-    labelPontos = fonte.render(str(qtdePontos), True, (255, 255, 255))
+        snake[0] = (snake[0][0] - TAM, snake[0][1])
+
+    labelPontos = fonteComicBranca.render(str(qtdePontos), True, (255, 255, 255))
+    labelLenghtQtde = fonteComicBranca.render(str(qtdeComprimento), True, (255, 255, 255))
+
     screen.fill((0,0,0))
-    screen.blit(labelScore, (10, 10))
-    screen.blit(labelPontos, (110, 10))
     screen.blit(apple, apple_pos)
-
-
-    linhaBranca = pygame.draw.line(screen, WHITE, [10,50], [590, 50], 2)
+    
+    borda1 = pygame.draw.line(screen, BORDA, [0,0], [650, 0], 40) #BORDA DE CIMA
+    borda2 = pygame.draw.line(screen, BORDA, [0,0], [0, 500], 40) #BORDA DA ESQUERDA
+    borda3 = pygame.draw.line(screen, BORDA, [650,500], [650, 0], 40) #BORDA DE BAIXO
+    borda4 = pygame.draw.line(screen, BORDA, [0,500], [650, 500], 90) #BORDA DA DIREITA
+    
+    screen.blit(labelLenght, (30, 470))
+    screen.blit(labelLenghtQtde, (110, 470))
+    screen.blit(labelScore, (500, 470))
+    screen.blit(labelPontos, (608, 470))
     pygame.display.flip()
     for pos in snake:
         screen.blit(snake_skin,pos)
